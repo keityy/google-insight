b0634c773324b67d0bb8324f77d225868f5b83d4
keito <youzhenjiuwo7@gmail.com>
2023-11-22 14:18:44 +0900

    3.Start Backend Development: Create a simple server using Express.js. Set up routes for user authentication and data import.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,4 +18,53 @@ app.get('/auth', (req, res) => {
 app.post('/import', (req, res) => {
     res.send('Data import route');
   });
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+
+// Configure session management
+app.use(session({ secret: 'anything', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+    // In this function, you'd handle the user's profile
+    // You could check if the user is in your database and create a user record if not
+    // For now, we'll just return the profile
+    return cb(null, profile);
+}
+));
+
+// Serialize and deserialize user instances to and from the session.
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
+
+//Create Authentication Routes:
+
+// Redirect the user to Google for authentication.
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// Google will redirect the user back to your app after authentication.
+// Handle the callback after Google has authenticated the user.
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
   
